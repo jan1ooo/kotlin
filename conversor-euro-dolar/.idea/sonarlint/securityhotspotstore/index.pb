f1934a566a418b4e15c367f0f84123e5a778c634
Janio Filho <filhojanio67@gmail.com>
2023-08-31 20:32:16 -0300

    Conversor Euro Dolar

---
+++ conversor-euro-dolar/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+p
+@app/src/main/java/com/jan1ooo/conversoreurodolar/MainActivity.kt,1\4\1446bdf3871dace48c0d767e6d21a5700634db6d
---
+++ conversor-euro-dolar/app/src/main/java/com/jan1ooo/conversoreurodolar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jan1ooo.conversoreurodolar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jan1ooo.conversoreurodolar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener{
+            val euros: Double = binding.editEuro.text.toString().toDouble()
+            val dolares = String.format("%.2f", euros * 0.8)
+            binding.textDolares.text = dolares + "$"
+        }
+    }
+
+
+}
